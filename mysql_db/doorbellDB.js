8dac154a5f59abdd05f907f558f86dd16ed2ed23
pnutz <pnutz0@gmail.com>
2015-01-06 20:32:56 -0800

    Altered db queries to use new db name.

--- mysql_db/doorbellDB.js
+++ mysql_db/doorbellDB.js
@@ -1,13 +1,13 @@
-module.exports.db = "CREATE DATABASE IF NOT EXISTS doorbelldb DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;";
-module.exports.use = "USE doorbelldb;";
+module.exports.db = "CREATE DATABASE IF NOT EXISTS heroku_25e074558aecc17 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;";
+module.exports.use = "USE heroku_25e074558aecc17;";
 
-module.exports.tables = ["CREATE TABLE IF NOT EXISTS doorbelldb.Status ( " +
+module.exports.tables = ["CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.Status ( " +
   "idStatus INT NOT NULL AUTO_INCREMENT, " +
   "Status VARCHAR(45) NOT NULL, " +
   "PRIMARY KEY (idStatus) " +
 "ENGINE = InnoDB;",
 
-"CREATE TABLE IF NOT EXISTS doorbelldb.User ( " +
+"CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.User ( " +
   "idUser INT NOT NULL AUTO_INCREMENT, " +
   "majorUuid INT NOT NULL, " +
   "minorUuid INT NOT NULL, " +
@@ -23,13 +23,13 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS doorbelldb.Status ( " +
   "UNIQUE INDEX deviceToken_UNIQUE (deviceToken ASC)) " +
 "ENGINE = InnoDB;",
 
-"CREATE TABLE IF NOT EXISTS doorbelldb.ErrorCode ( " +
+"CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.ErrorCode ( " +
   "idErrorCode INT NOT NULL AUTO_INCREMENT, " +
   "Error VARCHAR(45) NOT NULL, " +
   "PRIMARY KEY (idErrorCode)) " +
 "ENGINE = InnoDB;",
 
-"CREATE TABLE IF NOT EXISTS doorbelldb.Friend ( " +
+"CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.Friend ( " +
   "idFriend INT NOT NULL AUTO_INCREMENT, " +
   "idUser INT NOT NULL, " +
   "idFriendUser INT NOT NULL, " +
@@ -42,22 +42,22 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS doorbelldb.Status ( " +
   "INDEX idStatus_idx (idStatus ASC), " +
   "CONSTRAINT idUser " +
     "FOREIGN KEY (idUser) " +
-    "REFERENCES doorbelldb.User (idUser) " +
+    "REFERENCES heroku_25e074558aecc17.User (idUser) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION, " +
   "CONSTRAINT idFriend " +
     "FOREIGN KEY (idFriendUser) " +
-    "REFERENCES doorbelldb.User (idUser) " +
+    "REFERENCES heroku_25e074558aecc17.User (idUser) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION, " +
   "CONSTRAINT idStatus " +
     "FOREIGN KEY (idStatus) " +
-    "REFERENCES doorbelldb.Status (idStatus) " +
+    "REFERENCES heroku_25e074558aecc17.Status (idStatus) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION) " +
 "ENGINE = InnoDB;",
 
-"CREATE TABLE IF NOT EXISTS doorbelldb.Group ( " +
+"CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.Group ( " +
   "idGroup INT NOT NULL AUTO_INCREMENT, " +
   "idUser INT NOT NULL, " +
   "groupName VARCHAR(45) NOT NULL, " +
@@ -68,12 +68,12 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS doorbelldb.Status ( " +
   "INDEX idUser_idx (idUser ASC), " +
   "CONSTRAINT idUser " +
     "FOREIGN KEY (idUser) " +
-    "REFERENCES doorbelldb.User (idUser) " +
+    "REFERENCES heroku_25e074558aecc17.User (idUser) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION) " +
 "ENGINE = InnoDB;",
 
-"CREATE TABLE IF NOT EXISTS doorbelldb.GroupUser ( " +
+"CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.GroupUser ( " +
   "idGroupUser INT NOT NULL AUTO_INCREMENT, " +
   "idGroup INT NOT NULL, " +
   "idUser INT NOT NULL, " +
@@ -86,17 +86,17 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS doorbelldb.Status ( " +
   "INDEX idStatus_idx (idStatus ASC), " +
   "CONSTRAINT idGroup " +
     "FOREIGN KEY (idGroup) " +
-    "REFERENCES doorbelldb.Group (idGroup) " +
+    "REFERENCES heroku_25e074558aecc17.Group (idGroup) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION, " +
   "CONSTRAINT idUser " +
     "FOREIGN KEY (idUser) " +
-    "REFERENCES doorbelldb.User (idUser) " +
+    "REFERENCES heroku_25e074558aecc17.User (idUser) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION, " +
   "CONSTRAINT idStatus " +
     "FOREIGN KEY (idStatus) " +
-    "REFERENCES doorbelldb.Status (idStatus) " +
+    "REFERENCES heroku_25e074558aecc17.Status (idStatus) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION) " +
 "ENGINE = InnoDB;"];
