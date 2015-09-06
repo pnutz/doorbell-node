eccada8801b6d124a623024d89dfb69f591c4233
pnutz <pnutz0@gmail.com>
2015-09-06 12:56:10 -0700

    Moved group functionality to /old

--- mysql_db/doorbellDB.js
+++ mysql_db/doorbellDB.js
@@ -1,7 +1,8 @@
 module.exports.db = "CREATE DATABASE IF NOT EXISTS heroku_25e074558aecc17 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;";
 module.exports.use = "USE heroku_25e074558aecc17;";
 
-module.exports.tables = ["CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.Status ( " +
+module.exports.tables = [
+"CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.Status ( " +
   "idStatus INT NOT NULL AUTO_INCREMENT, " +
   "Status VARCHAR(45) NOT NULL, " +
   "PRIMARY KEY (idStatus)) " +
@@ -55,48 +56,5 @@ module.exports.tables = ["CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.Stat
     "REFERENCES heroku_25e074558aecc17.Status (idStatus) " +
     "ON DELETE NO ACTION " +
     "ON UPDATE NO ACTION) " +
-"ENGINE = InnoDB;",
-
-"CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.Group ( " +
-  "idGroup INT NOT NULL AUTO_INCREMENT, " +
-  "idUser INT NOT NULL, " +
-  "groupName VARCHAR(45) NOT NULL, " +
-  "uuid VARCHAR(32) NOT NULL, " +
-  "createdAt TIMESTAMP NOT NULL DEFAULT 0, " +
-  "updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
-  "PRIMARY KEY (idGroup), " +
-  "INDEX idUser_idx (idUser ASC), " +
-  "CONSTRAINT idUser2 " +
-    "FOREIGN KEY (idUser) " +
-    "REFERENCES heroku_25e074558aecc17.User (idUser) " +
-    "ON DELETE NO ACTION " +
-    "ON UPDATE NO ACTION) " +
-"ENGINE = InnoDB;",
-
-"CREATE TABLE IF NOT EXISTS heroku_25e074558aecc17.GroupUser ( " +
-  "idGroupUser INT NOT NULL AUTO_INCREMENT, " +
-  "idGroup INT NOT NULL, " +
-  "idUser INT NOT NULL, " +
-  "idStatus INT NOT NULL, " +
-  "createdAt TIMESTAMP NOT NULL DEFAULT 0, " +
-  "updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
-  "PRIMARY KEY (idGroupUser), " +
-  "INDEX idGroup_idx (idUser ASC), " +
-  "INDEX idUser_idx (idUser ASC), " +
-  "INDEX idStatus_idx (idStatus ASC), " +
-  "CONSTRAINT idGroup " +
-    "FOREIGN KEY (idGroup) " +
-    "REFERENCES heroku_25e074558aecc17.Group (idGroup) " +
-    "ON DELETE NO ACTION " +
-    "ON UPDATE NO ACTION, " +
-  "CONSTRAINT idUser3 " +
-    "FOREIGN KEY (idUser) " +
-    "REFERENCES heroku_25e074558aecc17.User (idUser) " +
-    "ON DELETE NO ACTION " +
-    "ON UPDATE NO ACTION, " +
-  "CONSTRAINT idStatus2 " +
-    "FOREIGN KEY (idStatus) " +
-    "REFERENCES heroku_25e074558aecc17.Status (idStatus) " +
-    "ON DELETE NO ACTION " +
-    "ON UPDATE NO ACTION) " +
-"ENGINE = InnoDB;"];
+"ENGINE = InnoDB;"
+];
